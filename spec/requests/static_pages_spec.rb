da818f83492692743af37cf8d2d2c33384118c66
Yilin Chen <wormful@gmail.com>
2013-07-31 21:17:26 +0800

    Refactor static pages spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "Microposts" }
+
   describe "Home page" do
     it "should have the content 'Microposts'" do
       visit '/static_pages/home'
@@ -9,7 +12,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Microposts | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -21,7 +24,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Microposts | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -33,7 +36,7 @@ describe "Static pages" do
 
     it "should have the title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Microposts | About")
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 
@@ -45,7 +48,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title('Contact')
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
