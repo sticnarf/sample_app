3da28bac97e1426ca7d3288a3bb33d2f3ee58592
Yilin Chen <wormful@gmail.com>
2013-08-08 21:27:08 +0800

    Add cucumber tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -55,7 +55,13 @@ describe "User Pages" do
 
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
       end
+
     end
   end
 end
